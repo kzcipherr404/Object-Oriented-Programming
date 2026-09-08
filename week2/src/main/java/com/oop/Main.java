6e7cb1892e99da25137bf0478bb66f9ed981fc67
kzcipherr404 <kondwanizzzz06@gmail.com>
2026-09-08 08:12:21 +0200

    Add attributes and methods to BankAccount

--- week2/src/main/java/com/oop/Main.java
+++ week2/src/main/java/com/oop/Main.java
@@ -5,6 +5,13 @@ public class Main {
         BankAccount jacobsAccount = new BankAccount();
         BankAccount useAccount = new BankAccount();
         System.out.println(jacobsAccount == useAccount);
+
+        jacobsAccount.accountHolder = "Jacob";
+        jacobsAccount.balance = 100;
+        System.out.println("Before deposit: " + jacobsAccount.balance);
+        jacobsAccount.deposit(100);
+        System.out.println("After deposit: " + jacobsAccount.balance);
+        System.out.println(useAccount.balance);
     }
 
 }
