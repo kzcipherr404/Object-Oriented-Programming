76fd59a3b0049073dba573aef434b233cc84731d
kzcipherr404 <kondwanizzzz06@gmail.com>
2026-09-09 11:27:19 +0200

    Add tests proving reference behaviour

---
+++ week3/src/test/java/com/oop/MemoryDemoTest.java
@@ -0,0 +1,30 @@
+package com.oop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MemoryDemoTest {
+
+    @Test
+    void separateObjectsDoNotShareState() {
+        // Arrange
+        BankAccount acc1 = new BankAccount();
+        BankAccount acc2 = new BankAccount();
+        // Act
+        acc1.balance = 500;
+        // Assert
+        assertEquals(0, acc2.balance);
+    }
+
+    @Test
+    void aliasedObjectsShareState() {
+        // Arrange
+        BankAccount acc1 = new BankAccount();
+        acc1.balance = 500;
+        BankAccount acc2 = acc1;
+        // Act
+        acc2.balance = 750;
+        // Assert
+        assertEquals(750, acc1.balance);
+    }
+}
